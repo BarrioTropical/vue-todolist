43a48444bfdeb364db4481e53a2f6bdbf90a6c5a
BarrioTropical <98409091+BarrioTropical@users.noreply.github.com>
2022-10-28 17:35:17 +0200

    creazione lista
    Signed-off-by: BarrioTropical <98409091+BarrioTropical@users.noreply.github.com>

--- js/script.js
+++ js/script.js
@@ -12,6 +12,55 @@ MILESTONE 3
 Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
-*/
+2- cliccando sul testo dell'item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)*/
 
+//const {createApp} = Vue;
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+createApp({
+    data(){
+        return {
+            newtask:'',
+            hasError: false,
+            listaSpesa:[
+                'Pane',
+                'Latte',
+                'Uova'
+            ]
+        }
+    },
+
+    methods:{
+        addTask(){
+            if(this.newtask.lnegth >=3){
+               this.listaSpesa.unshift(this.newtask);
+               this.hasError = false;
+            } else{
+                this.hasError = true;
+            }
+            this.newtask = '';
+        }
+    },
+
+    computed:{
+
+    },
+
+    mounted(){
+        console.log('in mounted');
+    },
+    created(){
+        console.log('in created');
+        console.log(this.surname);
+    },
+    beforeCreate(){
+        console.log('in before created');
+        console.log(this.surname);
+    },
+    beforeUpdate(){
+        console.log('in before update');
+    },
+    updated(){
+        console.log('in update');
+    }
+}).mount('#app')
